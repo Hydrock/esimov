8c19cf0bb8460e0b49b6a53137f501152d565be2
Aleksey <avechkanov@ourgapps.com>
2017-05-24 15:50:23 +0300

    add new component

---
+++ src/components/main-container/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.scss'
+
+class MainContainer extends Component {
+  render() {
+    return (
+      <div className="container">
+        asfasfasfsaf
+      </div>
+    );
+  }
+}
+
+export default MainContainer;
