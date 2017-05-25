03686b65484e8e0e5e87d86c40f7a35386c6528e
Aleksey <avechkanov@ourgapps.com>
2017-05-25 17:52:46 +0300

    add css modules

--- src/components/main-container/index.jsx
+++ src/components/main-container/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './style.scss'
+import style from './style.scss'
 
 class MainContainer extends Component {
   render() {
+    console.log(style)
     return (
-      <div className="container">
+      <div className={style.container}>
         asfasfasfsaf
       </div>
     );
