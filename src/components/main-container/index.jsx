69fd9967acd48393a438c392f3f1a2f526c82b2d
Aleksey <avechkanov@ourgapps.com>
2017-05-30 16:24:00 +0300

    add new img and random class

--- src/components/main-container/index.jsx
+++ src/components/main-container/index.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import style from './style.scss'
 
 class MainContainer extends Component {
+  randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)
+    rand = Math.round(rand);
+    return rand;
+  }
+
+  getStyle() {
+    var number = this.randomInteger(1, 3)
+    return classnames(style.container, style[`bg-${number}`])
+  }
+
   render() {
-    console.log(style)
     return (
-      <div className={style.container}>
+      <div className={this.getStyle()}>
         
       </div>
     );
