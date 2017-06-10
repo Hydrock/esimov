f7825ec632b0ad3ac7ae4897e48cc49b2c25744f
NatalieLoskutova <natalie.loskutova@gmail.com>
2017-06-10 04:56:39 +0300

    add ink effect

--- src/App.js
+++ src/App.js
@@ -13,13 +13,15 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        {/* <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */ }
+        {
+    /* <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p> */
+}
         <MainSlider/>
         <Logo/>
         <MainMenu/>
--- src/components/curtain/index.jsx
+++ src/components/curtain/index.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import style from './style.scss'
+import classnames from 'classnames'
 
 export default class Curtain extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      opening: false
+    }
+  }
+
+  setOpenState = () => {
+    this.setState(previousState => {
+      return { opening: !previousState.opening };
+    })
+  }
+
+  setAnimationClass () {
+    return this.state.opening ? style.opening : style.closing
+  }
+
   render () {
     return (
-      <div className={style.container}>
-        lorem
+      <div className={style.container} onClick={this.setOpenState}>
+        <div className={style.modal}></div>
+        <div className={classnames(style.transitionLayer, style.visible, this.setAnimationClass())}>
+            <div className={style.layer}></div>
+        </div>
       </div>
     )
   }
