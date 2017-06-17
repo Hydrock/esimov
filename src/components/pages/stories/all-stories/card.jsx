ef58960c339d48b77e378ce02b7b170a48c207f3
Alex Vechkanov <hydrock74@gmail.com>
2017-06-18 02:29:44 +0300

    add import images

--- src/components/pages/stories/all-stories/card.jsx
+++ src/components/pages/stories/all-stories/card.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import style from './style.scss'
 
+import img from '../../../../../src/assets/img/other/title.png'
+import storyImg from '../../../../../src/assets/img/other/story.jpg'
+import overlayImg from '../../../../../src/assets/img/other/black_overlay.png'
 
 export default class Card extends Component {
   render() {
@@ -9,10 +12,20 @@ export default class Card extends Component {
       <div className={style.card}>
         <div className={style.cardImageWrapper}>
           <img
-            src='http://i.annihil.us/u/prod/marvel/html_pages_assets/mmc-landing/images/cards/card-2-e4ef818a6a.jpg'
+            src={storyImg}
             className={style.cardImage}
+            alt=''
           />
-          <img className={style.cardTitleImage}/>
+          <img
+            src={overlayImg}
+            className={style.overlayImage}
+            alt=''
+          />
+          <img
+            src={img}
+            className={style.cardTitleImage}
+            alt=''
+          />
         </div>
         <div className={style.cardText}>
           Благодаря самому известному фотографу Оренбурга- сыну ветров, все чаще появляются спящие люди на улицах города. Свободные от стереотипов и всего остального мира они сами выбирают где, и когда им спать
