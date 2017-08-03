4277a96fd43646f1558dd69136e3590a41bb953e
Hydrock <hydrock74@gmail.com>
2017-08-03 13:58:48 +0300

    add webpack and preact

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,41 @@
 const path = require('path');
 const webpack = require('webpack');
 
+console.log(process.env.NODE_ENV)
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
-    app: './app.js',
+    app: './app.jsx',
   },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true },
+          },
+        ],
+      },
+      {
+        test: /\.jsx$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
 };
